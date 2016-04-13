ded96dde677bf4bae68b6574f5f297add5e56457
U-ACM1\ACM User <kmerfeld@gmial.com>
2016-04-13 14:09:55 -0500

    adding level 1

--- Assets/Scripts/PlayerController_minigame.cs
+++ Assets/Scripts/PlayerController_minigame.cs
@@ -7,6 +7,13 @@ public class PlayerController_minigame : MonoBehaviour {
 
 	private Rigidbody rb;
 
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
@@ -16,9 +23,16 @@ public class PlayerController_minigame : MonoBehaviour {
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		//float moveVertical = Input.GetAxis ("Vertical");
-
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
-
 		rb.AddForce (movement * speed);
+	}
+
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
 	}
 }
