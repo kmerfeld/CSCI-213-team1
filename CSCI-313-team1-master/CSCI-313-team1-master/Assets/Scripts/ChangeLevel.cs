807b0ebab79dbf06ba8993a0297fd9ea7d139df2
lcsstein <lcs_stein@yahoo.com>
2016-04-18 09:48:33 -0500

    added level3 and finished level1

---
+++ CSCI-313-team1-master/CSCI-313-team1-master/Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeLevel : MonoBehaviour {
+	public string Level;
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		SceneManager.LoadScene (Level);
+
+	}
+}
---
+++ CSCI-313-team1-master/CSCI-313-team1-master/README.md
@@ -0,0 +1,3 @@
+# CSCI-313-team1
+
+This is a game for our CSCI-313 project for spring 2016
