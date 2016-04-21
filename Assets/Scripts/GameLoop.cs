02e371d7d17a967f572431366f4b22d83cc67631
kyle merfeld <kmerfeld1@gmail.com>
2016-04-20 22:46:04 -0500

    Made minigame playable. added to HUD

--- Assets/Scripts/GameLoop.cs
+++ Assets/Scripts/GameLoop.cs
@@ -1,29 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class GameLoop : MonoBehaviour {
 
-	private int pops = 0;
-	public string Level;
+	public static int pops = 0;
+	public static int maxShots = 7;
+	public static int shots_taken;
+	public string nextLevel;
+	public string thisLevel;
+	public int toBreak;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		pops = 0;
 	}
-	
+	void OnGUI() {
+		GUI.Label(new Rect(10, 10, 1000, 200), "You have popped " + pops + "\\" + toBreak 
+			+ "\nYou have " + shots_taken + "\\" + maxShots + " Availiable");
+	}
 	// Update is called once per frame
 	void Update () {
-	
+		
+		if (pops == toBreak) {
+			shots_taken = 0;
+			pops = 0;
+			SceneManager.LoadScene (nextLevel);
+		}
+		if (GameLoop.maxShots == GameLoop.shots_taken) {
+			shots_taken = 0;
+			pops = 0;
+			SceneManager.LoadScene (thisLevel);
+			}
+
 	}
-
-
-	public void incrementPops ()
+	public void increment()
 	{
 		pops++;
-		if (pops == 3) {
-			SceneManager.LoadScene (Level);
-
-		}
 	}
+
+
 }
