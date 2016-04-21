03f600d91e6728ff9e092be782ca8256ec9b8858
kyle merfeld <kmerfeld1@gmail.com>
2016-04-21 08:52:38 -0500

    Added Test_mob

---
+++ Assets/Scripts/Test_Mob_follow.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test_Mob_follow : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	GameObject player;
+	Transform playerTransform;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+		
+	// Entity movement
+	void Update () {
+		GameObject player = GameObject.Find ("Player");
+		Transform playerTransform = player.transform;
+		// get player position
+		Vector3 position = playerTransform.position;
+
+		//move closer to senpai, I mean the player
+		if (playerTransform.position.x > rb.position.x)
+		{
+			rb.AddForce(new Vector2(100, 0));
+		}
+		if (playerTransform.position.x < rb.position.x)
+		{
+			rb.AddForce(new Vector2(-100, 0));
+		}
+	}
+
+
+	//check if player hits
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		//these two lines can probably not be here, but i was getting 
+		//null pointer without them
+		GameObject player = GameObject.Find ("Player");
+		Transform playerTransform = player.transform;
+
+		//if its player
+		if (coll.gameObject.tag == "Player") 
+		{
+			
+			//if the player is above
+			if (playerTransform.position.y > rb.position.y + 1) 
+			{
+				
+				Destroy (gameObject);
+
+			}
+			else {
+				//Hurt Player
+			}
+		}
+	}
+
+}
