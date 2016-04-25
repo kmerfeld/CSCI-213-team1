cda21fab211ab933d7e8fabb8a5c557c1404e83d
Clay Moody <claymdy15@gmail.com>
2016-04-25 13:59:42 -0500

    more stuff

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    private int score;
+    public GUIText scoreText;
+
+    void Start()
+    {
+        score = 0;
+        UpdateScore();
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        GetComponent<AudioSource>().Play();
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+	
+}
