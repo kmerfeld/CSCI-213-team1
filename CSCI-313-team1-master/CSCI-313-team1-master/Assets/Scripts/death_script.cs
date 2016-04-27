544005b782e85c4f8222bd8cbc014171dfbdb3f9
lcsstein <lcs_stein@yahoo.com>
2016-04-27 09:32:30 -0500

    adding levels 1,3

---
+++ CSCI-313-team1-master/CSCI-313-team1-master/Assets/Scripts/death_script.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class death_script : MonoBehaviour
+{
+    public string DeathScreen;
+    void OnCollisionEnter2D(Collider2D obj)
+    {
+        
+        /*if (obj.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene (DeathScreen);
+        }*/
+        Destroy(obj.gameObject);
+    }
+}
