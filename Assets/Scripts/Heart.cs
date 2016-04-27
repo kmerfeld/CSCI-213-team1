01c9260c117b48b730406cc8fb9e0c0562946ecf
Clay Moody <claymdy15@gmail.com>
2016-04-27 17:32:56 -0500

    Added heart collectible

---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityStandardAssets._2D
+{
+    public class Heart : MonoBehaviour
+    {
+        private PlatformerCharacter2D player;
+        
+        void Start()
+        {
+            GameObject playerObject = GameObject.FindWithTag("Player");
+            player = playerObject.GetComponent<PlatformerCharacter2D>();
+        }
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            
+            if (other.tag == "Player")
+            {
+                player.AddHealth(1);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
