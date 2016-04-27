33e0ae35fa5dc665b285cc5e0050e3711bb9253b
lcsstein <lcs_stein@yahoo.com>
2016-04-27 11:54:07 -0500

    Fixed my last fuckup

---
+++ Assets/Scripts/death_script.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class death_script : MonoBehaviour
+{
+    public string DeathScreen;
+    void OnCollisionEnter2D(Collision2D obj)
+    {
+        Destroy(obj.gameObject);
+        if (obj.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene (DeathScreen);
+        }
+        
+    }
+}
--- CSCI-313-team1-master/CSCI-313-team1-master/README.md
+++
@@ -1,3 +0,0 @@
-# CSCI-313-team1
-
-This is a game for our CSCI-313 project for spring 2016
