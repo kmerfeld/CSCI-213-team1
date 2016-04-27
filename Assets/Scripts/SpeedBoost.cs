c1ee8ce879039babf4e0272a46dfd255437b5c78
Clay Moody <claymdy15@gmail.com>
2016-04-27 18:25:03 -0500

    adding speed boost

---
+++ Assets/Scripts/SpeedBoost.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityStandardAssets._2D
+{
+    public class SpeedBoost : MonoBehaviour
+    {
+        private float modifier = 1.5f;
+        private PlatformerCharacter2D player;
+
+        void Start()
+        {
+            GameObject playerObject = GameObject.FindWithTag("Player");
+            player = playerObject.GetComponent<PlatformerCharacter2D>();
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.tag == "Player")
+            {
+                player.UpdateSpeed(modifier);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
