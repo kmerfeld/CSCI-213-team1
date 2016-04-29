40780be1b41392e40b62b4b09cad2dcf62b207e7
brimac1224 <briannamcdonald3@gmail.com>
2016-04-29 18:37:57 -0500

    Adding a Jump power up

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityStandardAssets._2D
+{
+    public class Jump : MonoBehaviour
+    {
+
+        private float modifier = 1.5f;
+        private PlatformerCharacter2D player;
+
+        void Start()
+        {
+            GameObject playerObject = GameObject.FindWithTag("Player");
+            player = playerObject.GetComponent<PlatformerCharacter2D>();
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                player.UpdateJump(modifier);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
