9b3b2556e46d94c6fba529453dfeed94fd495958
Claymdy15 <claymdy15@gmail.com>
2016-05-03 16:11:39 -0500

    added a level skip button (left alt)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
     private int score;
     public GUIText scoreText;
+
+    void Update()
+    {
+        bool pressed = Input.GetButtonDown("Fire2");
+        if (pressed)
+        {
+            nextlevel();
+        }
+    }
 
     void Start()
     {
@@ -21,6 +31,11 @@ public class GameController : MonoBehaviour {
     void UpdateScore()
     {
         scoreText.text = "Score: " + score;
+    }
+
+    void nextlevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
     }
 	
 }
